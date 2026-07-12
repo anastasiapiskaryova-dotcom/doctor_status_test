82624a9f8661cca8a78c982389e36c32efbf0643
anastasiapiskaryova-dotcom <anastasiapiskaryova@gmail.com>
2026-07-12 21:07:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ import os
 import re
 
 from aiogram import Bot, Dispatcher, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
